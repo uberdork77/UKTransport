70483859bac01aeefd281bc6bd30050633dffc5d
Kaerast <alice@kaerast.info>
2011-08-06 17:36:06 +0100

    make stops accessible from routes and vice versa

--- lib/uktransport.rb
+++ lib/uktransport.rb
@@ -3,19 +3,20 @@ require 'uktransport/cif'
 
 module UKTransport
   class Route
-    @stops = []
+    
     attr_accessor :operator, :journey, :start_date, :end_date, :weekdays, :school_holidays, :bankholidays, :route, :running_board, :vehicle_type, :registration_number, :direction, :stops
     def initialize
-    
+      @stops = []
     end
+    
   end
   
   class Stop
-    @routes = []
-    attr_accessor :location, :name, :gazetteer, :point_type, :gazetteer_id
+    
+    attr_accessor :location, :name, :gazetteer, :point_type, :gazetteer_id, :routes
   
     def initialize
-  
+      @routes = []
     end
   end
 end
