07a2bc0023d5bab98b58893b44a77a78d9b33330
Kaerast <alice@kaerast.info>
2011-08-06 15:26:53 +0100

    parse basic cif stop data

--- lib/uktransport/cif.rb
+++ lib/uktransport/cif.rb
@@ -4,15 +4,19 @@ module UKTransport
   class Cif
     @stops = []
     @@service_regex = /^QS([NDR])(.{4})(.{6})(\d{8})(\d{8})([01]{7})([SH ])([ ABX])(.{4})(.{6})(.{8})(.{8})(.)$/
+    @@location_regex = /^QL([NDR])(.{12})(.{48})(.)(.)(.{8})/
     attr_accessor :routes, :stops, :cif
     
 	def initialize(filename)
 	  @filename = filename
 	  @routes = []
+	  @stops = []
 	  datafile =  open(filename)
 	  datafile.readlines.each { |line|
 	    if ((line =~ /^QS/) == 0)
 	      @routes.push(line.scan(@@service_regex))
+	    elsif ((line =~ /^QL/) == 0)
+	      @stops.push(line.scan(@@location_regex))
 	    end
 	  }
 	end
